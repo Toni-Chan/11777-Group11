c762c37d053c6ba60a7316fe4fa62835f1a875d6
Scott <scott@scott-liu.com>
2020-11-03 21:02:43 -0500

    added model fetching script

---
+++ get_model.sh
@@ -0,0 +1,9 @@
+export MODEL_NAME=base-vg-labels
+export MODEL_DIR=model
+sudo apt-get update
+sudo apt-get install unzip
+mkdir $MODEL_DIR
+wget https://biglmdiag.blob.core.windows.net/oscar/pretrained_models/$MODEL_NAME.zip
+unzip $MODEL_NAME.zip -d $MODEL_DIR
+rm $MODEL_NAME.zip
+echo DONE
